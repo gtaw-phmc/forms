6a8d39c591ac7e547d77f0b6e6f2a9289544b7a9
LPX-Dev <73806627+LPX-Dev@users.noreply.github.com>
2025-08-31 02:31:12 +0100

    hotfix servicecomponent

--- src/components/ServiceUnavailable.js
+++ src/components/ServiceUnavailable.js
@@ -6,7 +6,7 @@ const ServiceUnavailable = () => {
     <div className="service-unavailable-overlay">
       <div className="service-unavailable-content">
                 <h1>Service Unavailable</h1>
-        <h2>As per the request of GTAW Management, this site is currently unavailable. I'm sorry for any inconvience caused.</h2>
+        <h2>As per the request of GTAW Management, this site is currently unavailable. I'm working with GTAW Management to resolve any outstanding requests.</h2>
       <h2> : - ( </h2>
       </div>
     </div>
